efca4650d1c7b2703e17d6846c57f79f26006f29
Bernhard.Lummerstorfer@gmail.com <Bernhard.Lummerstorfer@gmail.com@ed04737d-3637-5a06-cbeb-502f8ee4d442>
2011-05-09 23:53:05 +0000

    test log entry

--- trunk/src/com/googlecode/android_rtmp_broadcaster/AndroidRTMPBroadcaster.java
+++ trunk/src/com/googlecode/android_rtmp_broadcaster/AndroidRTMPBroadcaster.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class AndroidRTMPBroadcaster extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+  private static final String _LOG_TAG = 'main-activity'
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    Log.d(_LOG_TAG, "AndroidRTMPBroadcaster onCreate()");
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
 }
