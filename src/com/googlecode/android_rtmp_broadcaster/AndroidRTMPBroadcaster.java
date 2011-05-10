a49cc4886ecdf296ced80fe7f982b40163d5a48a
Bernhard.Lummerstorfer <Bernhard.Lummerstorfer@gmail.com>
2011-05-10 00:05:49 +0000

    android-rtmp-broadcaster says hello

--- src/com/googlecode/android_rtmp_broadcaster/AndroidRTMPBroadcaster.java
+++ src/com/googlecode/android_rtmp_broadcaster/AndroidRTMPBroadcaster.java
@@ -2,10 +2,11 @@ package com.googlecode.android_rtmp_broadcaster;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class AndroidRTMPBroadcaster extends Activity {
 
-  private static final String _LOG_TAG = 'main-activity'
+  private static final String _LOG_TAG = "main-activity";
 
   /** Called when the activity is first created. */
   @Override
